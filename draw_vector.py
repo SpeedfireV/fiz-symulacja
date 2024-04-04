d1733f2206b8470934230dcb2a2976b6d7d9619c
Grzan <grzegorz.niespodzianek@gmail.com>
2024-04-04 10:28:10 +0200

    arrow_angle_fixed

--- draw_vector.py
+++ draw_vector.py
@@ -9,12 +9,12 @@ def draw_vector(screen, start_pos, length, angle): # (length^2)/2 = x^2
     x = math.cos(angle) * length
     y = math.sin(angle) * length
     print(x,y)
-    end_pos = Vector2(start_pos.x + x, start_pos.y + y)
+    end_pos = Vector2(start_pos.x + x, start_pos.y - y)
     line(surface=screen, color=Color(255, 255, 255), start_pos=start_pos,
                      end_pos=end_pos, width=1)
 
-    arrow_1_angle = angle - math.pi/6 + math.pi
-    arrow_2_angle = angle + math.pi / 6 + math.pi
+    arrow_1_angle = -(angle - math.pi/6 + math.pi)
+    arrow_2_angle = -(angle + math.pi / 6 + math.pi)
 
     arrow_1_x = math.cos(arrow_1_angle) * 40
     arrow_1_y = math.sin(arrow_1_angle) * 40
@@ -23,7 +23,6 @@ def draw_vector(screen, start_pos, length, angle): # (length^2)/2 = x^2
 
     arrow_1_end = Vector2(end_pos.x + arrow_1_x, end_pos.y + arrow_1_y)
     arrow_2_end = Vector2(end_pos.x + arrow_2_x, end_pos.y + arrow_2_y)
-    print("arrow_1_end", arrow_1_end, "arrow_2_end", arrow_2_end)
 
     line(surface=screen, color=Color(255, 255, 255), start_pos=end_pos,
          end_pos=arrow_1_end, width=1)
