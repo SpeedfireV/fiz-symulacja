356b363b4ee06d4a6b30454e9468c744298aab4e
Grzegorz Niespodziany <grzegorz.niespodzianek@gmail.com>
2024-04-04 12:57:49 +0200

    Detecting mouse & Tickrate & Scale with 2 axes

--- cursor_pos.py
+++ cursor_pos.py
@@ -3,6 +3,6 @@ from pygame import Color
 
 
 def draw_cursor_pos(screen, screen_size, font, cursor_pos):
-    pygame.draw.rect(screen, color=Color('white'), rect=[1920 - 180, -1,1920,40], width=1)
+    pygame.draw.rect(screen, color=Color('white'), rect=[screen_size[0] - 180, -1,screen_size[0],40], width=1)
     text_surface = font.render(f'{cursor_pos[0]}, {cursor_pos[1]}', False, (255, 255, 255))
     screen.blit(text_surface, (screen_size[0] - 160, 5))
--- main.py
+++ main.py
@@ -5,24 +5,28 @@ from pygame import Vector2, Color
 
 from cursor_pos import draw_cursor_pos
 from draw_vector import draw_vector
+from scale_size import render_scale
 
 # pygame setup
 pygame.init()
 display_info = pygame.display.Info()
-screen_size = display_info.current_w, display_info.current_h
+screen_size = display_info.current_w - 1, display_info.current_h - 1
 screen = pygame.display.set_mode(screen_size)
 clock = pygame.time.Clock()
 running = True
-i = 10
 pygame.mouse.set_visible(False)
 pygame.font.init()
 font = pygame.font.SysFont('Times New Roman', 30)
-
+tickrate = 60
+current_scale = 100
 while running:
 
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pressed()[0]:
+                print("Left Mouse Button Clicked!")
         if event.type == pygame.QUIT:
             running = False
 
@@ -33,14 +37,13 @@ while running:
 
     draw_vector(screen=screen, start_pos=Vector2(500, 500), length=100, angle=3*math.pi/2)
     draw_cursor_pos(screen=screen,screen_size=screen_size,font=font, cursor_pos=cursor_pos)
-
+    render_scale(screen=screen, screen_size=screen_size, max_difference=100)
     is_clicked = pygame.mouse.get_pressed()[0]
 
     # RENDER YOUR GAME HERE
 
     # flip() the display to put your work on screen
     pygame.display.flip()
-    i += 1
-    clock.tick(60)  # limits FPS to 60
+    clock.tick(tickrate)  # limits FPS to 60
 
 pygame.quit()
