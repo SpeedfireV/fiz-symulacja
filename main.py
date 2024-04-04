dc51abc5363c7b0651bd4a2c82180beed650081f
Grzan <grzegorz.niespodzianek@gmail.com>
2024-04-04 09:59:07 +0200

    draw_vector

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+import math
+
 import pygame
+from pygame import Vector2, Color
+
+from draw_vector import draw_vector
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((1800, 900))
 clock = pygame.time.Clock()
 running = True
-
+i = 10
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -14,14 +19,14 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
-    pygame.draw.rect(pygame.Surface((0, 255)), color=pygame.Color(255,255,255), rect=pygame.Rect(10,30,40,70), width=30)
+    screen.fill(Color(20,20,20))
+    draw_vector(screen=screen, start_pos=Vector2(100, 100), length=100, angle=7*math.pi/4)
 
     # RENDER YOUR GAME HERE
 
     # flip() the display to put your work on screen
     pygame.display.flip()
-
+    i += 1
     clock.tick(60)  # limits FPS to 60
 
 pygame.quit()
