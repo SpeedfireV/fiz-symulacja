901cbb775fa76461e9b04f4964d171e1802273da
Grzan <grzegorz.niespodzianek@gmail.com>
2024-04-04 09:21:02 +0200

    pygame base

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("purple")
+    pygame.draw.rect(pygame.Surface((0, 255)), color=pygame.Color(255,255,255), rect=pygame.Rect(10,30,40,70), width=30)
+
+    # RENDER YOUR GAME HERE
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
